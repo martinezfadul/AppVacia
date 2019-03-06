bb0ef0b1e4101e2df381f2b8eb92647961c321a7
unknown <martinezfadul@gmail.com>
2019-03-06 14:47:34 -0600

    Initial commit

---
+++ app/src/main/java/com/selectos/temas/alumnos/appvacia/MainActivity.java
@@ -0,0 +1,22 @@
+package com.selectos.temas.alumnos.appvacia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View V)
+    {
+        Intent intenta_segunda_activity = new Intent(this,SegundaActivity.class);
+        startActivity(intenta_segunda_activity);
+    }
+}
